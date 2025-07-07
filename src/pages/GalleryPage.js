c5fbd4f81a04e4e0f8b7715efd4ee30f1d95edb4
Shrishti <shrishti.singh2111@gmail.com>
2025-07-07 18:53:06 +0530

    New add imges

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -3,21 +3,38 @@ import Modal from "react-bootstrap/Modal";
 import "../pages/GalleryPage.css";
 import backgroundImage from "../assets/sunset.jpeg";
 
-// Image imports
-import harita1 from "../assets/samtya.jpeg";
-import harita2 from "../assets/Haritanivas.jpeg";
-import harita3 from "../assets/chaityana.jpeg";
-import samatva1 from "../assets/chaityana.jpeg";
-import samatva2 from "../assets/samtya.jpeg";
-import samatva3 from "../assets/Haritanivas.jpeg";
-import sattva1 from "../assets/Haritanivas.jpeg";
-import sattva2 from "../assets/chaityana.jpeg";
-import chaitanya1 from "../assets/samtya.jpeg";
-import chaitanya2 from "../assets/chaityana.jpeg";
-import aamram1 from "../assets/samtya.jpeg";
-import aamram2 from "../assets/chaityana.jpeg";
-import panchbhooti1 from "../assets/chaityana.jpeg";
-import panchbhooti2 from "../assets/samtya.jpeg";
+import harita1 from "../assets/harita/Hari1.jpeg";
+import harita2 from "../assets/harita/Hari2.jpeg";
+import harita3 from "../assets/harita/Hari3.jpeg";
+import harita4 from "../assets/harita/Hari4.jpeg";
+import harita5 from "../assets/harita/Hari5.jpeg";
+import samatva1 from "../assets/samatva/samatva1.jpeg";
+import samatva2 from "../assets/samatva/samatva2.jpeg";
+import samatva3 from "../assets/samatva/samatva3.jpeg";
+import sattva1 from "../assets/sattva/sattva1.jpeg";
+import chaitanya1 from "../assets/chaityana/chaityana1.jpeg";
+import chaitanya2 from "../assets/chaityana/chaityana2.jpeg";
+import chaitanya3 from "../assets/chaityana/chaityana3.jpeg";
+import aam1 from "../assets/amaram/aam1.jpeg";
+import aam2 from "../assets/amaram/aam2.jpeg";
+import aam3 from "../assets/amaram/aam3.jpeg";
+import aam4 from "../assets/amaram/aam4.jpeg";
+import aam5 from "../assets/amaram/aam5.jpeg";
+import aam6 from "../assets/amaram/aam6.jpeg";
+import aam7 from "../assets/amaram/aam7.jpeg";
+import aam8 from "../assets/amaram/aam8.jpeg";
+import aam9 from "../assets/amaram/aam9.jpeg";
+import aam10 from "../assets/amaram/aam10.jpeg";
+import aam11 from "../assets/amaram/aam11.jpeg";
+import aam12 from "../assets/amaram/aam12.jpeg";
+import aam13 from "../assets/amaram/aam13.jpeg";
+import aam14 from "../assets/amaram/aam14.jpeg";
+import aam15 from "../assets/amaram/aam15.jpeg";
+import aam16 from "../assets/amaram/aam16.jpeg";
+import aam17 from "../assets/amaram/aam17.jpeg";
+import aam18 from "../assets/amaram/aam18.jpeg";
+import aam19 from "../assets/amaram/aam19.jpeg";
+import aam20 from "../assets/amaram/aam20.jpeg";
 import mridang1 from "../assets/chaityana.jpeg";
 import mridang2 from "../assets/samtya.jpeg";
 
@@ -25,19 +42,37 @@ const images = [
   harita1,
   harita2,
   harita3,
+  harita4,
+  harita5,
   samatva1,
   samatva2,
   samatva3,
   sattva1,
-  sattva2,
   chaitanya1,
   chaitanya2,
-  aamram1,
-  aamram2,
-  panchbhooti1,
-  panchbhooti2,
+  chaitanya3,
   mridang1,
   mridang2,
+  aam1,
+  aam2,
+  aam3,
+  aam4,
+  aam5,
+  aam6,
+  aam7,
+  aam8,
+  aam9,
+  aam10,
+  aam11,
+  aam12,
+  aam13,
+  aam14,
+  aam15,
+  aam16,
+  aam17,
+  aam18,
+  aam19,
+  aam20,
 ];
 
 const GalleryPage = () => {
@@ -77,27 +112,29 @@ const GalleryPage = () => {
               Gallery – Moments at Aranyaka
             </h1>
             <p className="text-center mb-5 lead text-muted">
-              Discover glimpses of life at Aranyaka — from serene sunrises to joyful
-              gatherings in nature’s embrace.
+              Discover glimpses of life at Aranyaka — from serene sunrises to
+              joyful gatherings in nature’s embrace.
             </p>
           </div>
         </div>
       </section>
-     <section>
-      <div className="row g-4 p-4">
-        {images.map((img, index) => (
-          <div className="col-sm-6 col-md-4 col-lg-3" key={index}>
-            <img
-              src={img}
-              alt={`Aranyaka ${index + 1}`}
-              className="img-fluid rounded shadow-sm gallery-img"
-              onClick={() => handleShow(index)}
-              style={{ cursor: "pointer" }}
-            />
-          </div>
-        ))}
-      </div>
-</section>
+
+      <section>
+        <div className="row g-4 p-4">
+          {images.map((img, index) => (
+            <div className="col-sm-6 col-md-4 col-lg-3" key={index}>
+              <img
+                src={img}
+                alt={`Aranyaka ${index + 1}`}
+                className="img-fluid rounded shadow-sm gallery-img"
+                onClick={() => handleShow(index)}
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+
       {/* Modal with navigation */}
       <Modal
         show={showModal}
@@ -105,7 +142,10 @@ const GalleryPage = () => {
         centered
         className="gallery-modal"
       >
-        <Modal.Body className="p-0 position-relative d-flex justify-content-center align-items-center" style={{ width: "50vw", height: "50vh", margin: "auto" }}>
+        <Modal.Body
+          className="p-0 position-relative d-flex justify-content-center align-items-center"
+          style={{ width: "50vw", height: "50vh", margin: "auto" }}
+        >
           <button
             className="btn btn-light position-absolute top-50 start-0 translate-middle-y"
             style={{ zIndex: 1050 }}
@@ -119,8 +159,8 @@ const GalleryPage = () => {
             style={{
               borderRadius: "10px",
               objectFit: "cover",
-              width: "100%",
-              height: "100%",
+              width: "176%",
+              height: "173%",
             }}
           />
           <button
--- src/pages/StayPage.js
+++ src/pages/StayPage.js
@@ -4,26 +4,44 @@ import Modal from "react-bootstrap/Modal";
 import backgroundImage from "../assets/amaram_nivas.jpeg";
 
 // Image Imports - Replace with actual files
-import harita1 from "../assets/samtya.jpeg";
-import harita2 from "../assets/Haritanivas.jpeg";
-import harita3 from "../assets/chaityana.jpeg";
+import harita1 from "../assets/harita/Hari1.jpeg";
+import harita2 from "../assets/harita/Hari2.jpeg";
+import harita3 from "../assets/harita/Hari3.jpeg";
+import harita4 from "../assets/harita/Hari4.jpeg";
+import harita5 from "../assets/harita/Hari5.jpeg";
 
-import samatva1 from "../assets/chaityana.jpeg";
-import samatva2 from "../assets/samtya.jpeg";
-import samatva3 from "../assets/Haritanivas.jpeg";
 
-import sattva1 from "../assets/Haritanivas.jpeg";
-import sattva2 from "../assets/chaityana.jpeg";
+import samatva1 from "../assets/samatva/samatva1.jpeg";
+import samatva2 from "../assets/samatva/samatva2.jpeg";
+import samatva3 from "../assets/samatva/samatva3.jpeg";
 
-import chaitanya1 from "../assets/samtya.jpeg";
-import chaitanya2 from "../assets/chaityana.jpeg";
+import sattva1 from "../assets/sattva/sattva1.jpeg";
 
-import aamram1 from "../assets/samtya.jpeg";
-import aamram2 from "../assets/chaityana.jpeg";
+import chaitanya1 from "../assets/chaityana/chaityana1.jpeg";
+import chaitanya2 from "../assets/chaityana/chaityana2.jpeg";
+import chaitanya3 from "../assets/chaityana/chaityana3.jpeg";
 
-import panchbhooti1 from "../assets/chaityana.jpeg";
-import panchbhooti2 from "../assets/samtya.jpeg";
 
+import aam1 from "../assets/amaram/aam1.jpeg";
+import aam2 from "../assets/amaram/aam2.jpeg";
+import aam3 from "../assets/amaram/aam3.jpeg";
+import aam4 from "../assets/amaram/aam4.jpeg";
+import aam5 from "../assets/amaram/aam5.jpeg";
+import aam6 from "../assets/amaram/aam6.jpeg";
+import aam7 from "../assets/amaram/aam7.jpeg";
+import aam8 from "../assets/amaram/aam8.jpeg";
+import aam9 from "../assets/amaram/aam9.jpeg";
+import aam10 from "../assets/amaram/aam10.jpeg";
+import aam11 from "../assets/amaram/aam11.jpeg";
+import aam12 from "../assets/amaram/aam12.jpeg";
+import aam13 from "../assets/amaram/aam13.jpeg";
+import aam14 from "../assets/amaram/aam14.jpeg";
+import aam15 from "../assets/amaram/aam15.jpeg";
+import aam16 from "../assets/amaram/aam16.jpeg";
+import aam17 from "../assets/amaram/aam17.jpeg";
+import aam18 from "../assets/amaram/aam18.jpeg";
+import aam19 from "../assets/amaram/aam19.jpeg";
+import aam20 from "../assets/amaram/aam20.jpeg";
 import mridang1 from "../assets/chaityana.jpeg";
 import mridang2 from "../assets/samtya.jpeg";
 import PackagesPage from "./PackagePage";
@@ -31,7 +49,7 @@ import PackagesPage from "./PackagePage";
 const stayData = [
   {
     name: "Harita Nivas – The Luxury Suite",
-    images: [harita1, harita2, harita3],
+    images: [harita1, harita2, harita3,harita4,harita5],
     description:
       "Our most spacious and elegant suite, Harita Nivas offers a blend of rustic charm and refined comfort. Adorned with natural textures, soft lighting, and a view that opens up to the orchard, it is perfect for those seeking tranquility with a touch of luxury.",
     features: [
@@ -55,20 +73,8 @@ const stayData = [
     ],
   },
   {
-    name: "Sattva Nivas – The Deluxe Room",
-    images: [sattva1, sattva2],
-    description:
-      "A serene hideaway with soothing earth-toned interiors, Sattva Nivas is our deluxe room designed for deep rest and rejuvenation. Overlooking the tranquil spa and the glistening swimming pool, this space invites you to unwind in stillness and reconnect with yourself.",
-    features: [
-      "Cozy double bed with organic linens",
-      "Spacious en-suite bathroom with natural stone finishes",
-      "Warm, earthy aesthetics with handcrafted accents",
-      "View of the spa and pool — perfect for sunrise reflections or starry nights",
-    ],
-  },
-  {
     name: "Chaitanya Nivas – Garden View Room",
-    images: [chaitanya1, chaitanya2],
+    images: [chaitanya1, chaitanya2,chaitanya3],
     description:
       "Bathed in natural light and opening to views of lush greenery, Chaitanya Nivas is a serene space that inspires reflection and quiet joy. A thoughtfully designed room with a king-sized bed and a beautiful aesthetic window framing the garden outside.",
     features: [
@@ -79,7 +85,7 @@ const stayData = [
   },
   {
     name: "Aamram Kutir – Mango Orchard Suite",
-    images: [aamram1, aamram2],
+    images: [aam1,aam3,aam4,aam6,aam11,aam12,aam14,aam15,aam16,aam19,aam20],
     description:
       "Overlooking the lush mango orchard, Aamram Kutir is a spacious suite ideal for families or those who enjoy extra room to stretch, breathe, and settle in. With its generous layout and tranquil views, it brings the feeling of a home tucked within nature.",
     features: [
@@ -88,10 +94,9 @@ const stayData = [
       "Large windows facing the mango orchard",
       "Rustic textures and quiet luxury",
     ],
-  },
-  {
+  }, {
     name: "Panchbhooti Kutir – Sky & Earth Suite",
-    images: [panchbhooti1, panchbhooti2],
+    images: [aam2,aam5,aam9,aam7,aam8,aam10,aam13,aam17,aam18],
     description:
       "This thoughtfully designed cottage offers both skyward inspiration and grounding earth energy. With views of the mango orchard and an open sky window that invites starlight in, Panchbhooti Kutir balances beauty, comfort, and elemental harmony.",
     features: [
@@ -101,6 +106,19 @@ const stayData = [
       "Two bathrooms for ease",
     ],
   },
+  {
+    name: "Sattva Nivas – The Deluxe Room",
+    images: [sattva1],
+    description:
+      "A serene hideaway with soothing earth-toned interiors, Sattva Nivas is our deluxe room designed for deep rest and rejuvenation. Overlooking the tranquil spa and the glistening swimming pool, this space invites you to unwind in stillness and reconnect with yourself.",
+    features: [
+      "Cozy double bed with organic linens",
+      "Spacious en-suite bathroom with natural stone finishes",
+      "Warm, earthy aesthetics with handcrafted accents",
+      "View of the spa and pool — perfect for sunrise reflections or starry nights",
+    ],
+  },
+ 
   {
     name: "Mridang Kutir – The Quiet Retreat",
     images: [mridang1, mridang2],
@@ -225,7 +243,7 @@ const RoomImageSlider = ({ images }) => {
           alt="room"
           className="img-fluid rounded shadow"
           onClick={() => setShowModal(true)}
-          style={{ cursor: "pointer", maxHeight: "300px", objectFit: "cover" }}
+          style={{ cursor: "pointer", maxHeight: "365px", objectFit: "cover" }}
         />
       </div>
 
